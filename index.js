c1100764432d1735d1200b5d1729a6a888eec1f4
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-10-21 15:39:28 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+  this.items = [];
+  this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b); 
+    this.length = this.items.length; 
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1]; 
+  }
+  
 
-  max() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+    return this.items[0]
+  }
+  
+  sum() {
+    let result = 0;
+    for(let i= 0;i<this.items.length;i++){
+      result+=this.items[i]
+    }
+    return result;
+  }
 
-  min() {}
+  avg() {
 
-  sum() {}
-
-  avg() {}
+    if (this.length === 0) {
+      throw new Error("EmptySortedList"); 
+    }
+    const total = this.sum(); 
+    return total / this.length; 
+  }
 }
 
 module.exports = SortedList;
